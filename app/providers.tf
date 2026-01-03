10494bc110f1b45b973c8e1fecb7adf1a99cd593
narlechitane38200 <narlechitane38200@gmail.com>
2026-01-03 11:11:37 +0100

    commit

--- app/providers.tf
+++ app/providers.tf
@@ -2,25 +2,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.10.0"
+      version = "= 6.26.0"
     }
   }
+  required_version = "1.14.2"
 
-  required_version = "1.9.4"
-
-  backend "s3" {
-    bucket                   = "tf-state-mini-project"
-    key                      = "mini-project.tfstate"
-    region                   = "us-east-1"
-    encrypt                  = true
-    dynamodb_table           = "terraform-state-lock"
-    shared_credentials_files = ["../.secrets/credentials"]
-    profile                  = "app"
-  }
+#  backend "s3" {
+#    bucket = "terraform-backend-radouane"
+#    key    = "demo_tfstate"
+#    region = "us-east-1"
+#    shared_credentials_files = [ "../.secrets/credentials" ]
+#    profile = "eazytraining-tp3"
+#  }
 }
 
+
 provider "aws" {
-  region                   = var.app_region
-  shared_credentials_files = ["../.secrets/credentials"]
-  profile                  = "app"
+  region = var.app_region
+  shared_credentials_files = [ "../.secrets/credentials" ]
+  profile = "radouane-mini-projet"
 }
